0887abe19c226f23f7180079f1029282be11e9fc
Ieven1 <vova.stoyanov.06@gmail.com>
2024-05-16 08:36:52 +0300

    .

---
+++ RP18.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, ""); 
+
+    wchar_t str[100];
+    int i = 0;
+    int toUpper = 1;
+    
+    wprintf(L"Введите текст: ");
+    fgetws(str, sizeof(str) / sizeof(wchar_t), stdin); 
+    
+    while (str[i] != L'\0') {
+        if ((str[i] >= L'a' && str[i] <= L'z') || (str[i] >= L'A' && str[i] <= L'Z')) { 
+            if (toUpper) {
+                if (str[i] >= L'a' && str[i] <= L'z') 
+                    str[i] -= 32; 
+            } else {
+                if (str[i] >= L'A' && str[i] <= L'Z') 
+                    str[i] += 32; 
+            }
+            toUpper = !toUpper; 
+        }
+        i++;
+    }
+
+    wprintf(L"Текст с измененным регистром: %ls\n", str);
+
+    return 0;
+}
